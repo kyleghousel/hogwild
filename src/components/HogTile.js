319d9136757e2910e72617a719f830f666ad387b
Kyle Housel <kylegranthousel@gmail.com>
2025-06-14 21:14:27 -0400

    Hide hogs

--- src/components/HogTile.js
+++ src/components/HogTile.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react"
 
 const HogTile = ({ id, name, weight, specialty, greased, image, onClick }) => {
+  const [isShowing, setIsShowing] = useState(true)
+
+  function handleHideClick(e) {
+    e.stopPropagation()
+    setIsShowing(prev => !prev)
+  }
+
+  if (!isShowing) return null
+
   return (
     <div className="ui eight wide column pigTile" id={id} onClick={onClick}>
       <h3 className="headerText">{name}</h3>
-      <img src={image} alt={`A pig named ${name}.`} className="minPigTile"/>
+      <img src={image} alt={`A pig named ${name}.`} className="minPigTile" />
+      <button onClick={handleHideClick}>Hide hog 🐖</button>
     </div>
   )
 }
